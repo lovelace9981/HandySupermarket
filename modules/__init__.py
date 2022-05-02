8a41dd7d6bb274aebc6b42a423f8f24222826920
Pedro Mayorgas <pedroamp@go.ugr.es>
2022-05-02 20:57:21 +0200

    Terminado el módulo de SQL con un contenedor propio. Así como el tratamiento de las consultas.

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,1 +1,4 @@
+# Indicas .fichero.py sin el .py y luego con import indicas la clase a cachear por __pycache__
+
 from .opencv import  camera, qrbarcode
+from .database import dmlContainer, databaseSQL
