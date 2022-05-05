cac4d66d01393054584942955f2abae5a7f5d06a
Pedro Mayorgas <pedroamp@go.ugr.es>
2022-05-05 20:05:33 +0200

    DML

---
+++ database/DML.sql
@@ -0,0 +1,9 @@
+INSERT INTO PASILLOS VALUES ("CARNICERIA01, Pasillo Carniceria","Este es el pasillo de carnicería donde tenemos productos frescos.", "img/pasillos/carniceria_pasillo.jpeg")
+
+INSERT INTO ESTANTES VALUES ("CARNICERIA01ES0","CARNICERIA01");
+
+INSERT INTO PRODUCTOS VALUES (1234567891011,"CARNICERIA01ES0", 10.5, "Bandeja de Pechuga de Pollo 1Kg", 10, "Proteína: 10mg, Grasas saturadas: 10mg","Carne de Pollo", "AVES", "img/productos/pechuga_pollo.jpg")
+
+INSERT INTO CLIENTES VALUES ("1", "Cliente 1", "Apellidos", "Calle Periodista")
+
+/*INSERT INTO COMPRAS VALUES ("LAST_ID_COMPRA+1", 1234567891011, 1, 1, 10, NOW())*/
