1a2bcbc1e548e7425c9a0e0a276b2b5d42eb9cf4
Adrian Acosa Sanchez <adrianacosa@correo.ugr.es>
2022-05-09 19:01:54 +0200

    Medio arreglada la camara

--- modules/database.py
+++ modules/database.py
@@ -2,8 +2,9 @@ import mariadb
 import sys
 import os
 
+
 class dmlContainer:
-    def __init__(self, header = None, body = None):
+    def __init__(self, header=None, body=None):
         self.__header = header
         self.__body = body
 
@@ -13,76 +14,76 @@ class dmlContainer:
     def getBodyTable(self):
         return self.__body
 
+
 class databaseSQL(object):
     __instance = None
 
     def __init__(self):
-            # inicio de comprobaciones
-            try:
-                self.__conn = mariadb.connect(
-                    user="USERHANDYSUPERMARKET",
-                    password="PASSWORD",
-                    #host="192.168.122.18",
-                    host="192.168.122.241",
-                    port=3306,
-                    database="HANDYSUPERMARKET"
+        # inicio de comprobaciones
+        try:
+            self.__conn = mariadb.connect(
+                user="USERHANDYSUPERMARKET",
+                password="PASSWORD",
+                #host="192.168.122.18",
+                #host="192.168.122.241",
+                host="192.168.122.105",
+                port=3306,
+                database="HANDYSUPERMARKET"
                 )
-            except mariadb.Error as e:
-                print(f"Error connecting to a MariaDB: {e}")
-                sys.exit(1)
+        except mariadb.Error as e:
+            print(f"Error connecting to a MariaDB: {e}")
+            sys.exit(1)
 
-            # Desactivamos los autocommit
-            self.__conn.autocommit = False
+        # Desactivamos los autocommit
+        self.__conn.autocommit = False
 
-            self.__cur = self.__conn.cursor()
+        self.__cur = self.__conn.cursor()
 
-            # Comprobamos que las tablas existen
-            self.__cur.execute("SHOW TABLES")
-            self.__cur.fetchall()
-            check_tables = self.__cur.rowcount
+        # Comprobamos que las tablas existen
+        self.__cur.execute("SHOW TABLES")
+        self.__cur.fetchall()
+        check_tables = self.__cur.rowcount
 
+        __path_ddl = os.getcwd() + "/modules/database/DDL.sql"
+        __path_dml = os.getcwd() + "/modules/database/DML.sql"
 
-            __path_ddl = os.getcwd() + "/modules/database/DDL.sql"
-            __path_dml = os.getcwd() + "/modules/database/DML.sql"
+        if check_tables == 0:
+            # Abrimos los ficheros de comprobacion de tablas de la base de datos
+            __ddl_file = open(__path_ddl, "r")
+            __ddl_text = __ddl_file.read()
+            __ddl_list = __ddl_text.split(";")
+            # Borramos el ultimo elemento de la lista que es un \n
+            __ddl_list.pop()
+            __ddl_file.close()
 
+            __dml_file = open(__path_dml, "r")
+            __dml_text = __dml_file.read()
+            __dml_list = __dml_text.split(";")
 
-            if check_tables == 0:
-                # Abrimos los ficheros de comprobacion de tablas de la base de datos
-                __ddl_file = open(__path_ddl, "r")
-                __ddl_text = __ddl_file.read()
-                __ddl_list = __ddl_text.split(";")
-                # Borramos el ultimo elemento de la lista que es un \n
-                __ddl_list.pop()
-                __ddl_file.close()
+            __dml_list.pop()
+            __dml_file.close()
 
-                __dml_file = open(__path_dml,"r")
-                __dml_text = __dml_file.read()
-                __dml_list = __dml_text.split(";")
+            try:
+                self.__conn.commit()
 
-                __dml_list.pop()
-                __dml_file.close()
-
-                try:
+                for i in __ddl_list:
+                    # execute DDL of tables
+                    self.__cur.execute(i)
+                    # Commit any pending transaction to database
                     self.__conn.commit()
 
-                    for i in __ddl_list:
-                        # execute DDL of tables
-                        self.__cur.execute(i)
-                        # Commit any pending transaction to database
-                        self.__conn.commit()
+                for i in __dml_list:
+                    # execute DDL of tables
+                    self.__cur.execute(i)
+                    # Commit any pending transaction to database
+                    self.__conn.commit()
 
-                    for i in __dml_list:
-                        # execute DDL of tables
-                        self.__cur.execute(i)
-                        # Commit any pending transaction to database
-                        self.__conn.commit()
+            except Exception as e:
+                print(f"Error executing a command: {e}")
+                self.__conn.rollback()
 
-                except Exception as e:
-                    print(f"Error executing a command: {e}")
-                    self.__conn.rollback()
-
-            # Destruimos el cursor para la
-            self.__cur.close()
+        # Destruimos el cursor para la
+        self.__cur.close()
 
     def __new__(cls):
         # Comprobacion de singleton
@@ -99,20 +100,20 @@ class databaseSQL(object):
     # TABLE INTO TABLE (COLUMN, COLUM2, COLUMN3)
     # CLAUSE VALUES (1,2,3) VALUE > 10
     # Boolean for check status of Transaction
-    def queryTransact(self, query = None, table = None, clause = None):
+    def queryTransact(self, query=None, table=None, clause=None):
         status = False
         try:
             # Check
             if (query != None and table != None and clause != None):
                 concatenateSQL = query + " " + table + " " + clause
-            else: # No hay comando de SQL Valido desde la entrada de string, esto no significa que el comando en sí sea correcto si no que es None
+            else:  # No hay comando de SQL Valido desde la entrada de string, esto no significa que el comando en sí sea correcto si no que es None
                 raise Exception("No se ha introducido el SQL correctamente")
         except Exception as e:
-            print (e.args)
+            print(e.args)
 
         try:
             # Creamos el cursor
-            self.__cur =  self.__conn.cursor()
+            self.__cur = self.__conn.cursor()
             # Execution
             self.__cur.execute(concatenateSQL)
             status = True
@@ -128,7 +129,7 @@ class databaseSQL(object):
     # QUERY SELECT *
     # TABLE FROM TABLE
     # CLAUSE WHERE VALUE > 10 OPCIONAL
-    def queryConsultas(self, query = None, table = None, clause = None):
+    def queryConsultas(self, query=None, table=None, clause=None):
         returndml = dmlContainer()
 
         try:
@@ -137,21 +138,21 @@ class databaseSQL(object):
                 concatenateSQL = query + " " + table
                 if (clause != None):
                     concatenateSQL += " " + clause
-            else: # No hay comando de SQL Valido desde la entrada de string, esto no significa que el comando en sí sea correcto si no que es None
+            else:  # No hay comando de SQL Valido desde la entrada de string, esto no significa que el comando en sí sea correcto si no que es None
                 raise Exception("No se ha introducido el SQL correctamente")
         except Exception as e:
-            print (e.args)
+            print(e.args)
 
         try:
             # Creamos el cursor
-            self.__cur =  self.__conn.cursor()
+            self.__cur = self.__conn.cursor()
             # Execution
             self.__cur.execute(concatenateSQL)
 
             # Obteniendo la informacion de la tabla
             fetch_return_body = self.__cur.fetchall()
             # Obteniendo los nombres de las columnas Cabecera de la tabla
-            fetch_return_header = [ x[0] for x in self.__cur.description ]
+            fetch_return_header = [x[0] for x in self.__cur.description]
             # Almacenando en contenedor para ser procesado fuera
             returndml = dmlContainer(fetch_return_header, fetch_return_body)
         except Exception as e:
--- modules/opencv.py
+++ modules/opencv.py
@@ -4,8 +4,9 @@ from pyzbar.pyzbar import decode
 
 import cv2
 
+
 class qrbarcode:
-    def __init__(self, inputdata = None, inputype = None):
+    def __init__(self, inputdata=None, inputype=None):
         self.__qrbarcodedata = inputdata
         self.__qrbarcodetype = inputype
 
@@ -14,6 +15,7 @@ class qrbarcode:
 
     def getType(self):
         return self.__qrbarcodetype
+
 
 class cameraScanner:
     def __init__(self, window):
@@ -27,20 +29,25 @@ class cameraScanner:
         if not self.cam.isOpened():
             raise Exception("No es posible abrir la camara", 0)
         # Resolucion de camara
-        #self.width = self.cam.get(cv2.CAP_PROP_FRAME_WIDTH)
-        #self.height = self.cam.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        self.width_height = 360
-
-        #self.cam.set(cv2.CAP_PROP_FRAME_WIDTH, self.width_height)
-        #self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, self.width_height)
+        self.width = self.cam.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.height = self.cam.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        # self.width_height = 360
+        #
+        # self.cam.set(cv2.CAP_PROP_FRAME_WIDTH, self.width_height)
+        # self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, self.width_height)
 
         # Intervalo de actualizacion
-        self.interval = 10 # Interval in ms to get the latest frame
+        self.interval = 10  # Interval in ms to get the latest frame
+
+        # self.cam.set(cv2.CAP_PROP_FRAME_WIDTH, self.width_height)
+        # self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, self.width_height)
 
         # Create canvas for image frame
         #self.canvas = tk.Canvas(self.window, width=self.width, height=self.height)
-        self.canvas = tk.Canvas(self.window, width=self.width_height, height=self.width_height)
-        self.canvas.grid(row=0, column=0)
+        self.canvas = tk.Canvas(
+            self.window, width=self.width, height=self.height)
+        self.canvas.place(x=-140, y=200)
+        # self.canvas.grid(row=0, column=0)
         # Update image on canvas
 #        self.update_image()
 
@@ -66,9 +73,10 @@ class cameraScanner:
         if self.cam.isOpened():
             self.frame = self.cam.read()[1]
             # Get the latest frame and convert image format
-            self.image = cv2.cvtColor(self.cam.read()[1], cv2.COLOR_BGR2RGB) # to RGB
-            self.image = Image.fromarray(self.image) # to PIL format
-            self.image = ImageTk.PhotoImage(self.image) # to ImageTk format
+            self.image = cv2.cvtColor(
+                self.cam.read()[1], cv2.COLOR_BGR2RGB)  # to RGB
+            self.image = Image.fromarray(self.image)  # to PIL format
+            self.image = ImageTk.PhotoImage(self.image)  # to ImageTk format
 
             # Update image
             self.canvas.create_image(0, 0, anchor=tk.NW, image=self.image)
